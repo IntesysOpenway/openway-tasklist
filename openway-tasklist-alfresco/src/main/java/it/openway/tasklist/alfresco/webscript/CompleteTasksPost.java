5ab78246f7ee321409472950bf5ae17e7f486831
Luca Rubin <luca.rubin@iopenway.it>
2017-02-16 16:03:16 +0100

    Aggiunta riferimenti di licenza a tutti i file di progetto
    - CHANGE: tramite procedimento automatico in fase di compilazione, tutti
    i file di progetto sono stati aggiornati con un header di licenza

--- openway-tasklist-alfresco/src/main/java/it/openway/tasklist/alfresco/webscript/CompleteTasksPost.java
+++ openway-tasklist-alfresco/src/main/java/it/openway/tasklist/alfresco/webscript/CompleteTasksPost.java
@@ -1,5 +1,28 @@
 package it.openway.tasklist.alfresco.webscript;
 
+/*-
+ * ==============================LICENSE=============================
+ * Intesys Openway Tasklist - Alfresco
+ * %%
+ * Copyright (C) 2016 - 2017 Intesys Openway Srl
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * ============================LICENSE-END===========================
+ */
+
+
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
